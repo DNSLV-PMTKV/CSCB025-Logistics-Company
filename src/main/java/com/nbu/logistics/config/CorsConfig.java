71ff37632a39843caf24d073ed4c9065c0e8a8df
DNSLV-PMTKV <pometkov.d@protonmail.com>
2021-05-16 12:49:54 +0300

    added cors

---
+++ src/main/java/com/nbu/logistics/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.nbu.logistics.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+	@Bean
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public FilterRegistrationBean<?> customCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		config.addAllowedOrigin("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<?> bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+}
